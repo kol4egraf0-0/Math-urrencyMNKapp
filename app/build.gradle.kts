bde769566056c4732adafca42c7afd2a1747c8cf
Evgen <genitya408@gmail.com>
2025-02-24 17:55:53 +0300

    Chart update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,10 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation ("com.google.code.gson:gson:2.8.9")
+
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
